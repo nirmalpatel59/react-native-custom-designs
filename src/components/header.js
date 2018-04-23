4e934a2be71687697f16a21be30855705901d040
Nirmal Patel <nirmal.patel@etouchmenu.com>
2018-04-23 19:57:00 +0530

    header menu icon integrated

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Header = (props) => {
-    const { viewStyle, textStyle } = styles;
-    return (
-        <View style={viewStyle}>
-            <Text style={textStyle}>{props.headerText}</Text>
+   const { viewStyle, headerView, iconView, textStyle } = styles
+   return (
+      <View style={headerView}>
+        <View style={iconView}>
+          <Icon name="menu" size={30} color="#000" />
         </View>
-    );
-};
+        <View style={viewStyle}>
+          <Text style={textStyle}>Albums</Text>
+        </View>
+        <View style={iconView}>
+        </View>
+      </View>
+    )
+ }
 
 const styles = {
-    viewStyle: {
-        backgroundColor: '#f8f8f8',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 60,
-        paddingTop: 5,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
-        elevation: 2,
-        position: 'relative'
-    },
-    textStyle: {
-        fontSize: 20
-    }
-};
-export default Header;
+  headerView: {
+    backgroundColor: '#f8f8f8',
+    // justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 6,
+    position: 'relative',
+    flexDirection: 'row'
+  },
+  iconView: {
+    padding: 16,
+    width: 72,
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+}
+export default Header
