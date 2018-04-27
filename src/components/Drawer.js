43b289a552c0a4d9de7b5887a49e2a7fd5e7cac7
Nirmal Patel <nirmal.patel@etouchmenu.com>
2018-04-27 14:44:38 +0530

    drawer design continue

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { DrawerLayoutAndroid, View, Text } from 'react-native';
+import { DrawerLayoutAndroid, FlatList, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 class Drawer extends Component {
   static drawerRef;
-  static openDrawer () {
+  static openDrawer() {
     drawerRef.openDrawer()
   }
 
@@ -12,21 +13,39 @@ class Drawer extends Component {
   }
 
   render() {
-  var navigationView = (
-    <View style={{flex: 1, backgroundColor: '#fff'}}>
-      <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
-    </View>
-  );
-  return (
-    <DrawerLayoutAndroid
-      drawerWidth={300}
-      drawerPosition={DrawerLayoutAndroid.positions.Left}
-      renderNavigationView={() => navigationView}
-      ref={ref => this.setDrawerRef(ref)}
-    >
-      {this.props.children}
-    </DrawerLayoutAndroid>
-  );
-}
+    const data = [{ key: 'first menu', icon: 'home', screen: 'home' }, { key: 'second menu' }, { type: 'separator' }, { 'key': 'settings' }]
+    
+    var navigationView = (
+      <View style={{padding: 16}}>
+        <FlatList
+          data={data}
+          renderItem={({ item }, index) => {
+            if (item.type === 'separator') {
+              return (
+                <View style={{ height: 0.5, backgroundColor: 'gray' }} />
+              )
+            }
+            return (
+              <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+                <Icon name={item.icon} size={15} color="gray" />
+                <Text key={index}>{item.key}</Text>
+              </View>
+            )
+          }}
+        />
+      </View>
+
+    );
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        renderNavigationView={() => navigationView}
+        ref={ref => this.setDrawerRef(ref)}
+      >
+        {this.props.children}
+      </DrawerLayoutAndroid>
+    );
+  }
 }
 export default Drawer;
